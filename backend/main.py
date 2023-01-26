ce2c611c8ef6553708aaa77ce43740d31fb02966
Buttee-bit <63400112+Buttee-bit@users.noreply.github.com>
2023-01-26 23:55:55 +0300

    [BD] add Register/JWT auth

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status, Depends
+from fastapi_users import FastAPIUsers
+from auth.auth import auth_backend
+from auth.schemas import UserRead, UserCreate
+from auth.DataBase import User
+from auth.manager import get_user_manager
+app = FastAPI ()
 
-app = FastAPI()
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
 
-fake_user = [
-    {}
-]
-
-@app.post("api/register")
-async def register():
-    return 
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
